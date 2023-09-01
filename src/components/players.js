802f19a558736b8a708ad80c2eb48e31a58694ba
dobbysock69 <103392445+dobbysock69@users.noreply.github.com>
2023-09-01 15:21:30 +0100

    added player.js and pictures

---
+++ src/components/players.js
@@ -0,0 +1,37 @@
+const players = [
+	{
+		name: "Lionel Messi",
+		team: "Inter Miami",
+		nationality: "Argentina",
+		jerseyNumber: 10,
+		age: 36,
+		imageUrl: "https://example.com/player1-image.jpg",
+	},
+	{
+		name: "Cristiano Ronaldo",
+		team: "Al Nassr",
+		nationality: "Portugal",
+		jerseyNumber: 7,
+		age: 38,
+		imageUrl: "https://example.com/player2-image.jpg",
+	},
+	{
+		name: "Kylian Mbappé",
+		team: "Paris Saint Germain",
+		nationality: "France",
+		jerseyNumber: 7,
+		age: 24,
+		imageUrl: "https://example.com/player3-image.jpg",
+	},
+	{
+		name: "Erling Haaland",
+		team: "Manchester City",
+		nationality: "Norway",
+		jerseyNumber: 9,
+		age: 23,
+		imageUrl:
+			"https://upload.wikimedia.org/wikipedia/commons/thumb/6/6e/Erling_Haaland_2023_%28cropped-v2%29.jpg/330px-Erling_Haaland_2023_%28cropped-v2%29.jpg",
+	},
+];
+
+export default players;
